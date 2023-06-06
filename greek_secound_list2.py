99045093aac1e4ba94c08ac1952231ddbdab729c
lokeshchadhokar <lokeshchadhokar9@gmail.com>
2023-06-06 10:10:54 +0530

    ho ja bhai

---
+++ greek_secound_list2.py
@@ -0,0 +1,7 @@
+'''Input : List = [23, 65, 19, 90], pos1 = 1, pos2 = 3
+Output : [19, 65, 23, 90]
+
+Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
+Output : [1, 5, 3, 4, 2]
+simple swap, using comma assignment
+Since the positions of the elements are known, we can simply swap the positions of the element'''
