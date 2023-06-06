15744da4a53a5a3acbb936bae6883a71f1e8b8f2
lokeshchadhokar <lokeshchadhokar9@gmail.com>
2023-06-06 09:58:16 +0530

    simple swap, using comma assignment

---
+++ greek questions.py
@@ -0,0 +1,15 @@
+'''Given a list, write a Python program to swap first and last element of the list.
+
+Examples:
+
+Input : [12, 35, 9, 56, 24]
+Output : [24, 35, 9, 56, 12]'''
+input = [12, 35, 9, 56, 24]
+a=input.pop(0)
+b=input.pop(-1)
+print(input)
+c=len(input)
+
+input.insert(0,b)
+input.insert(-1,a)
+print(input)
