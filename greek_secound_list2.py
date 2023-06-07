b28da2198b5961e6d2ad18bc2879cb5e1b04e8a4
Sulekha18 <sulekhasingh267@gmailcom>
2023-06-07 14:47:34 +0530

    greek

--- greek_secound_list2.py
+++ greek_secound_list2.py
@@ -5,3 +5,4 @@ Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
 Output : [1, 5, 3, 4, 2]
 simple swap, using comma assignment
 Since the positions of the elements are known, we can simply swap the positions of the element'''
+print('hello new world')
