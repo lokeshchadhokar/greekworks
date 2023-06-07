fda52c9d1898e19336b205c2f2620e939e67e89e
lokeshchadhokar <lokeshchadhokar9@gmail.com>
2023-06-07 11:06:43 +0530

    the swap the last and first number succed also i define a function

--- greek questions.py
+++ greek questions.py
@@ -1,15 +1,33 @@
 '''Given a list, write a Python program to swap first and last element of the list.
 
-Examples:
-
 Input : [12, 35, 9, 56, 24]
 Output : [24, 35, 9, 56, 12]'''
-input = [12, 35, 9, 56, 24]
-a=input.pop(0)
-b=input.pop(-1)
-print(input)
-c=len(input)
+input = [12, 35, 9, 56, 24]#list type
+print('original list',input)
+c=len(input)#its contain length of list
+a=input.pop(0)#12 pop working like remove the given indexing occurance from container and store in diff veriable
+b=input.pop()#24 pop by default remove last indexing of acept tuple list,set,etc containet
+input.insert(0,b)# insert working fill/add the given data on the bases of indexing place
+input.insert(c,a)#
+print("swap last and first number ",input)
 
-input.insert(0,b)
-input.insert(-1,a)
-print(input)
+
+#Examples:
+
+'''
+list : [12, 35, 9, 56, 24]
+Output : [24, 35, 9, 56, 12]'''
+# make a function
+def swap(list):
+    c=len(list)#its contain length of list
+    print("orignal list is : ",list)
+    a=list.pop(0)#12 pop working like remove the given indexing occurance from container and store in diff veriable
+    b=list.pop()#24 pop by default remove last indexing of acept tuple list,set,etc containet
+    list.insert(0,b)# insert working fill/add the given data on the bases of indexing place
+    list.insert(c,a)#
+    #print("swap last and first number :")
+    return "swap last and first number :",list
+li= [10,20,30,40,50]
+print(swap(li))
+
+
--- greek_secound_list2.py
+++ greek_secound_list2.py
@@ -3,5 +3,5 @@ Output : [19, 65, 23, 90]
 
 Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
 Output : [1, 5, 3, 4, 2]
-simple swap, using comma assignment
+
 Since the positions of the elements are known, we can simply swap the positions of the element'''
